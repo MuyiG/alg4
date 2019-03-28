436943bf8d10f098c79d6700c8606ba722927e5e
杨光 <yangguang01@weidian.com>
2019-03-28 19:13:18 +0800

    buildGraph

--- src/BoggleSolver.java
+++ src/BoggleSolver.java
@@ -9,18 +9,26 @@ public class BoggleSolver {
 
     // Returns the set of all valid words in the given Boggle board, as an Iterable.
     public Iterable<String> getAllValidWords(BoggleBoard board) {
+        int m = board.rows(), n = board.cols();
+        Graph graph = buildGraph(m, n);
+        boolean[] marked = new boolean[m * n];
+        int[] edgeTo = new int[m * n];
 
+        for (int v = 0; v < graph.V(); v++) {
+
+        }
 
         return null;
     }
 
-    // FIXME
-    private Graph buildGraph(BoggleBoard board) {
-        int m = board.rows(), n = board.cols();
+    private Graph buildGraph(int m, int n) {
         Graph graph = new Graph(m * n);
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 int v = i * m + j;
+                if (i > 0 && j < n - 1) {
+                    graph.addEdge(v, (i - 1) * m + j + 1);
+                }
                 if (i < m - 1) {
                     graph.addEdge(v, (i + 1) * m + j);
                     if (j < n - 1) {
@@ -43,7 +51,6 @@ public class BoggleSolver {
 
     public static void main(String[] args) {
         BoggleSolver solver = new BoggleSolver(null);
-        BoggleBoard board = new BoggleBoard(3, 3);
-        System.out.println(solver.buildGraph(board));
+        System.out.println(solver.buildGraph(3, 3));
     }
 }
