1a578ec963f553ba0a708101abec32ef7315887f
杨光 <sunshine.vvv@163.com>
2019-04-15 00:25:41 +0800

    非递归写法

--- src/PrefixTrie.java
+++ src/PrefixTrie.java
@@ -1,3 +1,4 @@
+import edu.princeton.cs.algs4.In;
 
 public class PrefixTrie<Value> {
     private static final int R = 26;        // A-Z
@@ -18,6 +19,7 @@ public class PrefixTrie<Value> {
 
     /**
      * Returns the number of key-value pairs in this symbol table.
+     *
      * @return the number of key-value pairs in this symbol table
      */
     public int size() {
@@ -26,6 +28,7 @@ public class PrefixTrie<Value> {
 
     /**
      * Is this symbol table empty?
+     *
      * @return {@code true} if this symbol table is empty and {@code false} otherwise
      */
     public boolean isEmpty() {
@@ -34,62 +37,86 @@ public class PrefixTrie<Value> {
 
     /**
      * Returns the value associated with the given key.
+     *
      * @param key the key
      * @return the value associated with the given key if the key is in the symbol table
-     *     and {@code null} if the key is not in the symbol table
+     * and {@code null} if the key is not in the symbol table
      * @throws NullPointerException if {@code key} is {@code null}
      */
     public Value get(String key) {
-        Node x = get(root, key, 0);
-        if (x == null) return null;
+//        Node x = get(root, key, 0);
+//        if (x == null) return null;
+//        return (Value) x.val;
+
+        // 非递归写法
+        Node x = root;
+        for (char c : key.toCharArray()) {
+            if (x == null || x.next[c - 'A'] == null) {
+                return null;
+            }
+            x = x.next[c - 'A'];
+        }
         return (Value) x.val;
     }
 
     /**
      * Does this symbol table contain the given key?
+     *
      * @param key the key
      * @return {@code true} if this symbol table contains {@code key} and
-     *     {@code false} otherwise
+     * {@code false} otherwise
      * @throws NullPointerException if {@code key} is {@code null}
      */
     public boolean contains(String key) {
         return get(key) != null;
     }
 
-    private Node get(Node x, String key, int d) {
-        if (x == null) return null;
-        if (d == key.length()) return x;
-        char c = key.charAt(d);
-        return get(x.next[c - 'A'], key, d+1);
-    }
+//    private Node get(Node x, String key, int d) {
+//        if (x == null) return null;
+//        if (d == key.length()) return x;
+//        char c = key.charAt(d);
+//        return get(x.next[c - 'A'], key, d+1);
+//    }
 
     /**
      * Inserts the key-value pair into the symbol table, overwriting the old value
      * with the new value if the key is already in the symbol table.
      * If the value is {@code null}, this effectively deletes the key from the symbol table.
+     *
      * @param key the key
      * @param val the value
      * @throws NullPointerException if {@code key} is {@code null}
      */
     public void put(String key, Value val) {
         if (val == null) delete(key);
-        else root = put(root, key, val, 0);
+//        else root = put(root, key, val, 0);
+
+        // 非递归写法
+        Node x = root;
+        for (char c : key.toCharArray()) {
+            if (x.next[c - 'A'] == null) {
+                x.next[c - 'A'] = new Node();
+            }
+            x = x.next[c - 'A'];
+        }
+        x.val = val;
     }
 
-    private Node put(Node x, String key, Value val, int d) {
-        if (x == null) x = new Node();
-        if (d == key.length()) {
-            if (x.val == null) n++;
-            x.val = val;
-            return x;
-        }
-        char c = key.charAt(d);
-        x.next[c - 'A'] = put(x.next[c - 'A'], key, val, d+1);
-        return x;
-    }
+//    private Node put(Node x, String key, Value val, int d) {
+//        if (x == null) x = new Node();
+//        if (d == key.length()) {
+//            if (x.val == null) n++;
+//            x.val = val;
+//            return x;
+//        }
+//        char c = key.charAt(d);
+//        x.next[c - 'A'] = put(x.next[c - 'A'], key, val, d+1);
+//        return x;
+//    }
 
     /**
      * Removes the key from the set if the key is present.
+     *
      * @param key the key
      * @throws NullPointerException if {@code key} is {@code null}
      */
@@ -102,10 +129,9 @@ public class PrefixTrie<Value> {
         if (d == key.length()) {
             if (x.val != null) n--;
             x.val = null;
-        }
-        else {
+        } else {
             char c = key.charAt(d);
-            x.next[c - 'A'] = delete(x.next[c - 'A'], key, d+1);
+            x.next[c - 'A'] = delete(x.next[c - 'A'], key, d + 1);
         }
 
         // remove subtrie rooted at x if it is completely empty
@@ -129,5 +155,13 @@ public class PrefixTrie<Value> {
         }
 
         return true;
+    }
+
+    public static void main(String[] args) {
+        PrefixTrie<Integer> prefixTrie = new PrefixTrie<>();
+        prefixTrie.put("ABC", 1);
+        prefixTrie.put("CBA", 1);
+        System.out.println(prefixTrie.contains("CBA"));
+        System.out.println(prefixTrie.contains("NBA"));
     }
 }
